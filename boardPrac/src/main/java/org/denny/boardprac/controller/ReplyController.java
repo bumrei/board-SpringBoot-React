d2c8e19f9c77597b3d638d3d2c9cf74787f1f93c
bumrei <bumrei@gmail.com>
2022-03-18 16:20:30 +0900

    REPLY elimination and modification logics are completed and all those logics are adapted to the view side. [Working Check is completed]

--- boardPrac/src/main/java/org/denny/boardprac/controller/ReplyController.java
+++ boardPrac/src/main/java/org/denny/boardprac/controller/ReplyController.java
@@ -6,10 +6,7 @@ import org.denny.boardprac.dto.PageRequestDTO;
 import org.denny.boardprac.dto.PageResponseDTO;
 import org.denny.boardprac.dto.ReplyDTO;
 import org.denny.boardprac.service.ReplyService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +21,35 @@ public class ReplyController {
 
 
         return replyService.getListOfBoard(bno, pageRequestDTO);
+    }
+
+    @PostMapping("")
+    public PageResponseDTO<ReplyDTO> register(@RequestBody ReplyDTO replyDTO) {
+
+        replyService.register(replyDTO); // 댓글 등록하고나서
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(-1).build();
+
+        return replyService.getListOfBoard(replyDTO.getBno(), pageRequestDTO); // 댓글 새로운 정보 불러오기
+
+    }
+
+    @DeleteMapping("/{bno}/{rno}")
+    public PageResponseDTO<ReplyDTO> remove(@PathVariable("bno") Long bno , @PathVariable("rno") Long rno, PageRequestDTO pageRequestDTO) {
+
+        return replyService.remove(bno, rno, pageRequestDTO);
+
+    }
+
+    @PutMapping("/{bno}/{rno}")
+    public PageResponseDTO<ReplyDTO> modify(@PathVariable("bno") Long bno, @PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO, PageRequestDTO pageRequestDTO) {
+
+        log.info("bno : " + bno);
+        log.info("rno : " + rno);
+        log.info("replyDTO : " + replyDTO);
+
+        return replyService.modify(replyDTO, pageRequestDTO);
+
     }
 
 }
